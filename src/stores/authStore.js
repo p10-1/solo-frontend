4752b771a82e02a060b2fbb0f83dac4d821ae1dd
SogCheonBul <jy6443@naver.com>
2024-09-21 21:03:10 +0900

    Fix: authStore에서 JSON parsing 부분 수정

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -3,14 +3,16 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    userInfo: null
+    userInfo: JSON.parse(sessionStorage.getItem('userInfo')) || null // 세션 스토리지에서 사용자 정보 로드
   }),
   actions: {
     setUserInfo(user) {
       this.userInfo = user
+      sessionStorage.setItem('userInfo', JSON.stringify(user)) // 세션 스토리지에 저장
     },
     clearUserInfo() {
       this.userInfo = null
+      sessionStorage.removeItem('userInfo') // 세션 스토리지에서 제거
     }
   },
   getters: {
