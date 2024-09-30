18075a3537ff8e4da51d0b25cc9b42293475b43f
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-09-30 13:35:25 +0900

    Bring: dev

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -17,7 +17,7 @@ export const useAuthStore = defineStore('auth', {
   },
   getters: {
     isLoggedIn: (state) => !!state.userInfo,
-    username: (state) => (state.userInfo ? state.userInfo.name : '')
+    username: (state) => (state.userInfo ? state.userInfo.nickName : '')
     // userID: (state) => (state.userInfo ? state.userInfo.kakaoId : '')
   }
 })
